4426a68cb909714fd6ac8f5d1ef88fda1881a18a
jesusg@seas.upenn.edu <jesug@seas.upenn.edu>
2016-11-25 19:32:52 -0500

    Update

--- cumMinEngVer.m
+++ cumMinEngVer.m
@@ -3,7 +3,7 @@ function [Mx, Tbx] = cumMinEngVer(e)
 % Mx is the cumulative minimum energy map along vertical direction.
 % Tbx is the backtrack table along vertical direction.
 
-%edit
+%% Setup
 [ny,nx] = size(e);
 Mx = zeros(ny, nx);
 Tbx = zeros(ny, nx);
@@ -12,19 +12,17 @@ Mx(1,:) = e(1,:);
 %there is also a cummin option
 %Mx = cummin(e,1);
 
+%% Compute Mx and Tbx
 for i = 2:ny
-    for j = 2:nx-1
-        Mx(i,j) = e(i,j)+ min(Mx(i-1,j-1),Mx(i-1,j),Mx(i-1,j+1));
+    for j = 1:nx
+        if j~=1
+            [val ind] = min([Mx(i-1,j-1),My(i-1,j),My(i-1,j+1)]);
+        else
+            [val ind] = min([Mx(i-1,j),My(i-1,j+1)]);
+            ind = ind+1;
+        end
+        Tbx(i,j) = ind;
+        Mx(i,j) = e(i,j)+ val;
     end
-end
-%min entry on Mx in the last row
-[min_entry,ind] = min(Mx(ny,:));
-
-%backtrack from min entry on Mx
-for i = ny:-1:2
-    backing = [Mx(i-1,ind-1) M(i-1,ind) M(i-1,ind+1)];
-    [value,inc] = min(backing);
-    min_entry = min_entry + value;
-    ind = ind + inc -2;
 end
 end
