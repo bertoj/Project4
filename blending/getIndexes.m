ddd5c7ca6e44db97d3179c821b5795af6cc6604d
jesusg@seas.upenn.edu <jesug@seas.upenn.edu>
2016-11-27 22:23:14 -0500

    Started blend

---
+++ blending/getIndexes.m
@@ -0,0 +1,19 @@
+function indexes = getIndexes(mask, targetH, targetW, offsetX, offsetY)
+
+    % initialize matrix size
+    indexes = zeros(targetH, targetH);
+
+    mask2 = padarray(mask,[offsetY, offsetX],'pre');
+    
+    [row col] = size(mask2);
+    
+    mask3 = padarray(mask2,[targetH-row,targetW-col],'post');
+
+    [y x] = find(mask3);
+    sorted_pairs = sortrows([y x]);
+    ind = sub2ind(size(mask3),sorted_pairs(:,1),sorted_pairs(:,2));
+    indexes(ind(1)) = mask3(ind(1));
+    for i=2:length(ind)
+       indexes(ind(i)) =  mask3(ind(i)) + indexes(ind(i-1));
+    end
+end
