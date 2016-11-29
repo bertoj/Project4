7c6746e9753a2506a583b2316f0b39b5649d4b81
jesusg@seas.upenn.edu <jesug@seas.upenn.edu>
2016-11-28 23:29:25 -0500

    update

--- blending/maskImage.m
+++
@@ -1,8 +0,0 @@
-function mask = maskImage(Img)
-
-image = imshow(Img);
-mask_region = imfreehand;
-mask = createMask(mask_region,image);
-
-end
-
--- blending/test.m
+++
@@ -1,9 +0,0 @@
-close all; clear; clc;
-
-
-sourceImg = imread('bear.png');
-targetImg = imread('pool.png');
-offsetX = 25;
-offsetY = 150;
-mask = maskImage(sourceImg);
-resultImg = seamlessCloningPoisson(sourceImg, targetImg, mask, offsetX, offsetY);
