67cb5ff73b2166d10beeef92e08fecb07e117340
jesusg@seas.upenn.edu <jesug@seas.upenn.edu>
2016-12-08 14:30:05 -0500

    gg

--- PartB/HoG.m
+++ PartB/HoG.m
@@ -1,9 +1,35 @@
 close all; clear;clc
 
-img = imread('cameraman.tif');
-[featureVector,hogVisualization] = extractHOGFeatures(img);
+vidObj = VideoReader('easy1.mp4');
 
-subplot(1,2,1);
-imshow(img);
-subplot(1,2,2);
-plot(hogVisualization);
+vidHeight = vidObj.Height;
+vidWidth = vidObj.Width;
+
+s = struct('cdata',zeros(vidHeight,vidWidth,3,'uint8'),...
+    'colormap',[]);
+
+
+k = 1;
+while hasFrame(vidObj)
+    s(k).cdata = rgb2gray(readFrame(vidObj));
+    [H{k},theta{k},rho{k}] = hough(s(k).cdata);
+    k = k+1;
+end
+
+% image(s(90).cdata)
+imshow(s(90).cdata);
+
+% 
+% set(gcf,'position',[150 150 vidObj.Width vidObj.Height]);
+% set(gca,'units','pixels');
+% set(gca,'position',[0 0 vidObj.Width vidObj.Height]);
+% movie(s,1,vidObj.FrameRate);
+
+% 
+% img = imread('cameraman.tif');
+% [featureVector,hogVisualization] = extractHOGFeatures(img);
+% 
+% subplot(1,2,1);
+% imshow(img);
+% subplot(1,2,2);
+% plot(hogVisualization);
