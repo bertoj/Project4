4a0549f03aaaa6c8cbf9cdb7ff2e13177bee4e52
jesusg@seas.upenn.edu <jesug@seas.upenn.edu>
2016-12-20 19:46:43 -0500

    face, titties, booty and toes

--- PartB/createCodebook.m
+++ PartB/createCodebook.m
@@ -11,23 +11,30 @@ BW = edge(trainingImage,'canny');
 CB = struct('edgeMap',[],'feature',[],'distance',[],'offset',[],'pos',[]);
 
 % Creat nose mask
-[x_c,y_c,P] = impixel(trainingImage);
+[x_c,y_c,P] = impixel(BW);
+close all;
 X = [x_c-width x_c+width x_c+width x_c-width x_c-width];
 Y = [y_c-length y_c-length y_c+length y_c+length y_c-length];
 mask = poly2mask(X,Y,m,n);
-
-% nose edgemap
-CB(1).edgeMap = mask.*BW; %Nose
-eMap = imcrop(CB(1).edgeMap,[x_c-width,y_c-length,33,33]);
-% nose HoG vect0r
-% [featureVector,hogVisualization] = extractHOGFeatures(eMap);
-CB(1).feature = featureVector/sqrt(norm(featureVector)^2+.001);
-
-
 % nose distance from center
 CB(1).distance = 0;
 CB(1).offset = [0,0];
 CB(1).pos = [x_c,y_c];
+% nose edgemap
+CB(1).edgeMap = mask.*BW; %Nose
+eMap = imcrop(CB(1).edgeMap,[x_c-width,y_c-length,33,33]);
+[x_c,y_c,P] = impixel(eMap);
+close all;
+% nose HoG vect0r
+% [featureVector,hogVisualization] = extractHOGFeatures(eMap);
+figure;
+I = find(eMap);
+[row,col] = ind2sub(size(eMap),I);
+SC = getSC([row,col],[x_c,y_c],.1,20,8,4);
+
+
+CB(1).feature = featureVector/sqrt(norm(featureVector)^2+.001);
+
 for i = 2:5
 % Creat feature mask
 [x,y,P] = impixel(trainingImage);
